96ed29c3a65ce018f6a163ac2bf7460d40f97f69
Ariel Costas <a21arielcg@iesteis.es>
2022-11-04 12:59:21 +0100

    Limpiar proyecto

--- bibliotecasApp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from bibliotecasApp.models import Cliente
-
-# Register your models here.
-admin.site.register(Cliente)
--- bibliotecasApp/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def lista_clientes(request):
-	return render(request, 'clientes.html')
