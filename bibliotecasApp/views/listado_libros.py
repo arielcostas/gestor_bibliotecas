48e1e26d202f29ed21829bb280ad65650cd23bd9
Ariel Costas <94913521+arielcostas@users.noreply.github.com>
2022-11-11 12:46:58 +0100

    Cambios

--- bibliotecasApp/views/listado_libros.py
+++ bibliotecasApp/views/listado_libros.py
@@ -6,4 +6,5 @@ from bibliotecasApp.models import Libro
 def listado_libros(request):
 	"""Listado de libros"""
 	libros = Libro.objects.all()
+	print(libros)
 	return render(request, 'index.html', {'libros': libros})
