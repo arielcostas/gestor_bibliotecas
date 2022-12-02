2a5559f71255b7280e8d73ddc90e57c4a1d32a4f
Ariel Costas <94913521+arielcostas@users.noreply.github.com>
2022-12-02 12:47:17 +0100

    Retoques

--- bibliotecasApp/models/prestamo.py
+++ bibliotecasApp/models/prestamo.py
@@ -5,7 +5,7 @@ class Prestamo(models.Model):
 	id = models.AutoField(primary_key=True)
 	fecha_prestamo = models.DateField()
 	fecha_devolucion = models.DateField()
-	fecha_devolucion_real = models.DateField()
+	fecha_devolucion_real = models.DateField(blank=True, null=True)
 	socio = models.ForeignKey('Socio', on_delete=models.CASCADE)
 	libro = models.ForeignKey('Libro', on_delete=models.CASCADE)
 
