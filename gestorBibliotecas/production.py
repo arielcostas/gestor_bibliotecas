46567f09b4f17e94dd8973052ace661e409c8c9a
Ariel Costas <94913521+arielcostas@users.noreply.github.com>
2022-12-17 23:37:42 +0100

    Añadir whitenoise

--- gestorBibliotecas/production.py
+++ gestorBibliotecas/production.py
@@ -3,6 +3,18 @@ import os
 
 DEBUG = False
 
+MIDDLEWARE = [
+	'django.middleware.security.SecurityMiddleware',
+	'whitenoise.middleware.WhiteNoiseMiddleware',
+	'django.contrib.sessions.middleware.SessionMiddleware',
+	'django.middleware.common.CommonMiddleware',
+	'django.middleware.csrf.CsrfViewMiddleware',
+	'django.contrib.auth.middleware.AuthenticationMiddleware',
+	'django.contrib.messages.middleware.MessageMiddleware',
+	'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 hostname = os.environ['DBHOST']
